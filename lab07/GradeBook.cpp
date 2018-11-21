9497d1424bb8226ab218f46d8edb2ba89b5272c2
Heecatee <heecatee67@gmail.com>
2018-11-21 16:28:43 +0100

    Update GradeBook.cpp

--- lab07/GradeBook.cpp
+++ lab07/GradeBook.cpp
@@ -7,15 +7,15 @@ GradeBook::GradeBook(){}
 
 void GradeBook::insert(const Grade* grade){
   if (grade->getId() == 1) {
-		gradeList_ = new const Grade*[1];
-		gradeList_[0] = grade;
-	}
-	else {
+    gradeList_ = new const Grade*[1];
+    gradeList_[0] = grade;
+  }
+  else {
     const Grade** tmp = gradeList_;
-		gradeList_ = new const Grade*[grade->getId()];
+    gradeList_ = new const Grade*[grade->getId()];
     
     for (int i = 0; i <= size_; i++)
-			gradeList_[i] = tmp[i];
+      gradeList_[i] = tmp[i];
     
     gradeList_[size_] = grade;
 
