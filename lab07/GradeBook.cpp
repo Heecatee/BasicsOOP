238a3ea6822b71b1cf913594382f460399b0ebd0
Heecatee <heecatee67@gmail.com>
2018-11-25 13:36:28 +0100

    Update GradeBook.cpp

--- lab07/GradeBook.cpp
+++ lab07/GradeBook.cpp
@@ -2,37 +2,37 @@
 using namespace std;
 
 GradeBook::GradeBook(){
-  size_ = 0;
+		size_ = 0;
 }
 
 void GradeBook::insert(const Grade* grade){
-  const Grade** tmp = gradeList_;
-  gradeList_ = new const Grade*[size_ + 1];
+    const Grade** tmp = gradeList_;
+    gradeList_ = new const Grade*[size_ + 1];
      
-  for (int i = 0; i < size_; i++)
-    gradeList_[i] = tmp[i];
+    for (int i = 0; i < size_; i++)
+        gradeList_[i] = tmp[i];
     
-  gradeList_[size_] = grade;
+    gradeList_[size_] = grade;
 
-  if(size_)
-    delete[] tmp;
+    if(size_)
+        delete[] tmp;
   
-  ++size_;
+    ++size_;
 }
 
 void GradeBook::addGrade(float grade){
-  Grade* newGrade = new Grade(grade);
-  insert(newGrade);
+    Grade* newGrade = new Grade(grade);
+    insert(newGrade);
 }
 
 void GradeBook::print() const{
-  cout << "Grades in the book:" << endl;
-  for (int i = 0; i < size_; i++) 
-    gradeList_[i]->print();
+    cout << "Grades in the book:" << endl;
+    for (int i = 0; i < size_; i++) 
+        gradeList_[i]->print();
 }
 
 GradeBook::~GradeBook(){
-  for (int i = 0; i < size_; i++) 
-    delete[] gradeList_[i];
-  delete[] gradeList;
+    for (int i = 0; i < size_; i++) 
+        delete[] gradeList_[i];
+    delete[] gradeList;
 }
